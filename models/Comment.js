dc377fc81e4c8d3dd0f4f5c765efb445975464c0
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-07-06 08:07:37 -0700

    adding reply schema to the comment model

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,43 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
 
-const CommentSchema = new Schema({
+const ReplySchema = new Schema(
+    {
+        //set custome id to avoid confusion with parent comment id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+
+        replyBody: {
+            type: String
+        },
+
+        writtenBy: {
+            type: String
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    },
+
+    //Adding this to be able toaccept getters======================================
+    {
+        toJSON: {
+          getters: true
+        }
+       
+    }
+
+
+)
+
+const CommentSchema = new Schema(
+{
+
     writtenBy: {
         type: String
     },
@@ -11,10 +48,32 @@ const CommentSchema = new Schema({
 
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+
+    replies: [ReplySchema]
+
+},
+
+//Adding this to be able toaccept virtuals======================================
+{
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
     }
+
+);
+
+// get total count of replies on retrieval
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
+
+
 
 module.exports = Comment;
